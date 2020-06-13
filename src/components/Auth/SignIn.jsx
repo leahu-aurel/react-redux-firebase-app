43bafe3cd49b85b34630a4774db3b4b6fda32863
Aurel Leahu <aurel.leahu@intern.arobs.com>
2020-06-13 05:43:53 +0300

    Added Sign in and Signup components and css for them

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,3 +1,27 @@
-import React from "react";
-
-export default () => <h1>Sign in</h1>;
+import React, { useRef } from "react";
+import "./auth.css";
+export default () => {
+  let login = useRef();
+  let password = useRef();
+  const submitHandle = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="authContainer">
+      <br />
+      <h1>Sign in</h1>
+      <form onSubmit={submitHandle}>
+        <label>
+          Login <br /> <input ref={login} />
+        </label>
+        <br />
+        <label>
+          Password
+          <input ref={password} type="password" />
+        </label>
+        <br />
+        <button className="buttonContainer">Log in</button>
+      </form>
+    </div>
+  );
+};
--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -1,3 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 
-export default () => <h1>Sign up</h1>;
+import "./auth.css";
+export default () => {
+  let login = useRef();
+  let password = useRef();
+  let password2 = useRef();
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="authContainer">
+      <br />
+      <h1>Sign up</h1>
+      <form onSubmit={submitHandle}>
+        <label>
+          Login <br /> <input ref={login} />
+        </label>
+        <br />
+        <label>
+          Password
+          <input ref={password} type="password" />
+        </label>
+        <br />
+
+        <label>
+          Repeat password
+          <input ref={password2} type="password" />
+        </label>
+        <br />
+        <button className="buttonContainer">Log in</button>
+      </form>
+    </div>
+  );
+};
